286be5928e6c7d125a317a5601c7c427a0769731
chankruze <chankruze@gmail.com>
2019-04-02 11:05:22 +0530

    Initial Stable Push

---
+++ js/contributors.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.contributors-logos').slick({
+        adaptiveHeight: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
